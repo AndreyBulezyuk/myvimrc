4734e3b9e0d8cf890e976cc3990adf4c185ca8fd
Andrey Bulezyuk <andrey.income@gmail.com>
2017-12-19 18:03:55 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,5 @@
-cd /etc/vim/ && git clone xy
+cp vimrc ~/.vim/vimrc
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/skielbasa/vim-material-monokai.git ~/.vim/
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
